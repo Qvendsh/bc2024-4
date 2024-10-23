ed3e72bb2fad1a9b049a311965ac61d917ce02d8
Qvendsh bc2024-4 <qvendsh@gmail.com>
2024-10-23 19:49:47 +0300

    part 2 done

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
-
 const http = require('http');
+const fs = require('fs/promises');
+const path = require('path');
 const { program } = require('commander');
 
-
 program
-    .version('1.0.0')
-    .description('A simple command-line application for a web server')
-    .requiredOption('-h, --host <type>', 'Server address')
-    .requiredOption('-p, --port <number>', 'Server port')
-    .requiredOption('-c, --cache <path>', 'Path to cache directory')
-    .parse(process.argv);
+  .requiredOption('-h, --host <type>', 'server address')
+  .requiredOption('-p, --port <number>', 'server port')
+  .requiredOption('-c, --cache <path>', 'cache directory');
 
+program.parse(process.argv);
 
 const { host, port, cache } = program.opts();
 
-console.log(`Server will run at http://${host}:${port}`);
-console.log(`Cache directory: ${cache}`);
+fs.mkdir(cache, { recursive: true }).catch(console.error);
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
+const server = http.createServer(async (req, res) => {
+  const urlParts = req.url.split('/');
+  const code = urlParts[1];
+
+  if (req.method === 'GET') {
+    try {
+      const filePath = path.join(cache, `${code}.jpg`);
+      const data = await fs.readFile(filePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } catch (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  } else if (req.method === 'PUT') {
+    const chunks = [];
+    req.on('data', chunk => chunks.push(chunk));
+    req.on('end', async () => {
+      const filePath = path.join(cache, `${code}.jpg`);
+      const buffer = Buffer.concat(chunks);
+      await fs.writeFile(filePath, buffer);
+      res.writeHead(201, { 'Content-Type': 'text/plain' });
+      res.end('Created');
+    });
+  } else if (req.method === 'DELETE') {
+    try {
+      const filePath = path.join(cache, `${code}.jpg`);
+      await fs.unlink(filePath);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Deleted');
+    } catch (error) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method Not Allowed');
+  }
 });
 
 server.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`);
+  console.log(`Server running at http://${host}:${port}/`);
 });
+
